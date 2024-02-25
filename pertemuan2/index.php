34724c5a1e6581d122ec86f8950b3a3991bad849
sandiarbaa <sandiarbaa@gmail.com>
2024-02-25 14:41:36 +0700

    2. Sintaks PHP

---
+++ pertemuan2/index.php
@@ -0,0 +1,55 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka.
+// $nama = "Sandi Arba Putra";
+// echo $nama;
+// echo 'Halo, Nama Saya $nama';
+// echo "Halo, Nama Saya $nama";
+
+// penggabung string / concatenation /  concat
+// .
+
+// $nama_depan = "Sandi";
+// $nama_belakang = "Arba";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Asignment
+// = += -= *= /= %= .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+
+// $nama = "Sandi";
+// $nama .= " ";
+// $nama .= "Arba";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 != "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+// var_dump(1 !== "1");
+
+// Logika
+// && || !
+// $x = 30;
+// var_dump($x > 20 && $x % 2 == 0);
+// var_dump($x < 20 || $x % 2 == 0);
+// var_dump($x <= 30 || $x > 30 && $x % 2 == 0);
+?>
