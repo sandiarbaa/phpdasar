ca24b321305ad4cbb53bf678e8c580eff95e4f8b
sandiarbaa <sandiarbaa@gmail.com>
2024-03-12 15:29:34 +0700

    14. Registrasi

---
+++ pertemuan14/index.php
@@ -0,0 +1,68 @@
+<?php
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if( isset($_POST["cari"]) ) {
+    $mahasiswa = cari($_POST["keyword"]);
+    // cek apakah hasil pencarian kosong
+    if (empty($mahasiswa)) {
+        echo "
+            <script>
+                alert('Maaf, data tidak ditemukan!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan nim/nama/email/prodi" autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>
+    <br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Prodi</th>
+        </tr>
+        
+        <?php $i = 1; ?>
+        <?php foreach( $mahasiswa as $row) : ?>
+        <tr>
+            <td><?= $i; ?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+                <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('anda yakin ingin menghapus data ini?');">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"]; ?>" alt="gambar"></td>
+            <td><?= $row["nim"]; ?></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["email"]; ?></td>
+            <td><?= $row["prodi"]; ?></td>
+        </tr>
+        <?php $i++; ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
